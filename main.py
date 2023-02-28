863c176d90b560746b2cb36910d26800abd3d952
mm-news <105084@mhps.tp.edu.tw>
2023-02-28 20:17:06 +0800

    fix bug

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from mitmproxy.tools.main import mitmdump
 
 input("Please remember to run web.py! If you did, press ENTER> ")
-mitmdump(args=["-s", "./workers/proxy.py"])
+mitmdump(args=["-s", "./proxy/proxy.py"])
