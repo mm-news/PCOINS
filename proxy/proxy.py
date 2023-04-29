dd5353faed91ab65ef784a8789c38bcee2c8b1eb
mmtv <mmtv.news.tw@gmail.com>
2023-04-29 13:49:07 +0800

    Update proxy.py

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -1,23 +1,10 @@
 from mitmproxy import http
 
 def request(flow: http.HTTPFlow) -> None:
-    """request"""
+    '''request'''
 
 def response(flow: http.HTTPFlow) -> None:
-    from bs4 import BeautifulSoup
-    
-    if flow.response and flow.response.content:
-        body_tag = BeautifulSoup(flow.response.content)
-        body_tag.find("body")
-        print(body_tag)
-        flow.response.content = flow.response.content.replace("<body>".encode(), """<body>
-        <h1>Ha!</h1>
-        <script>
-            function altf() {
-                alert("HA!")
-                altf()
-            }
-            alert("HA!")
-            altf()
-        </script>
-        """.encode())
+    from functions.functions import getconfig
+    body_end = flow.response.content.find(b'</body>')
+    replace = open("replace.txt").read().replace(r"{message}", getconfig("string", "message_when_blocked"))
+    flow.response.content = flow.response.content[:body_end] + replace.encode() + flow.response.content[body_end:]
