703641901f051e1cd61ad7889dc27e5997c5556b
Undefined <77557596+mm-news@users.noreply.github.com>
2023-05-03 12:39:30 +0000

    rename wen.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from mitmproxy.tools.main import mitmdump
 
-input("Please remember to run web.py! If you did, press ENTER> ")
+input("Please remember to run web_worker.py! If you did, press ENTER> ")
 mitmdump(args=["-s", "./proxy/proxy.py"])
--- web.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from web import routes
-
-app = Flask(__name__)
-
-from functions import functions
-
-if __name__ == "__main__":
-    app.run()
