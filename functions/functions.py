44a01c3ce4e5d7dd25e251a845e9b0d9830e26dd
Undefined <77557596+mm-news@users.noreply.github.com>
2023-05-03 12:40:48 +0000

    add levels

--- functions/functions.py
+++ functions/functions.py
@@ -9,3 +9,25 @@ def getconfig(area:str, name:str):
         return config[area]
     else:
         return config
+
+def edit_html(html:bytes) -> bytes:
+    from functions.functions import getconfig
+    body_end = html.find(b'</body>')
+    replace = open(getconfig("files", "replace.txt")).read().replace(r"{message}", getconfig("string", "message_when_blocked"))
+    return html[:body_end] + replace.encode() + html[body_end:]
+
+def get_hosts_csv() -> dict:
+    from functions.functions import getconfig
+    from pandas import read_csv
+
+    csv = read_csv(getconfig("files", "hosts_list")[1:-1])
+
+    hosts = {}
+
+    for i in csv.values:
+        hosts[i[0]] = {
+            "level": i[1],
+            "reason": i[2],
+            "comment": i[3]
+        }
+    return hosts
--- proxy/proxy.py
+++ proxy/proxy.py
@@ -1,10 +1,23 @@
+'''Proxy Scripts'''
 from mitmproxy import http
 
 def request(flow: http.HTTPFlow) -> None:
     '''request'''
 
 def response(flow: http.HTTPFlow) -> None:
-    from functions.functions import getconfig
-    body_end = flow.response.content.find(b'</body>')
-    replace = open("replace.txt").read().replace(r"{message}", getconfig("string", "message_when_blocked"))
-    flow.response.content = flow.response.content[:body_end] + replace.encode() + flow.response.content[body_end:]
+    '''response'''
+    if "text/html" in flow.response.headers["content-type"]:
+        import functions.functions as functions
+
+        host = flow.request.host
+
+        if host in functions.get_hosts_csv():
+            level = functions.get_hosts_csv()[host]["level"]
+            if level <= 0:
+                return
+            elif level >= 1:
+                flow.response.content = functions.edit_html(flow.response.content)
+                return
+        else:
+            return
+            
